85d57473f37e5c5c6170df2eea38497977799a1c
keno555552 <k024g1024@g.neec.ac.jp>
2025-12-15 11:48:36 +0900

    02-01 v1.0

--- 02-01/main.cpp
+++ 02-01/main.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
-#include <stdio.h>
+#include <iomanip>
+
+/// <summary>
+/// ‘Î”äŠÖ”(¬‚³‚¢‚Ù‚¤‚ğ•Ô‚·)
+/// </summary>
+/// <typeparam name="T">Œ`</typeparam>
+/// <param name="a">‘Î”ä‘ÎÛa</param>
+/// <param name="b">‘ÎÛ‘ÎÛb</param>
+/// <returns>¬‚³‚¢‚Ù‚¤</returns>
+template<typename T>
+T Compare(T& a, T& b) {
+	if (a < b) return a;
+	else return b;
+}
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+
+	/// intŒ^‚Ì”äŠr
+	{
+		int i = 10;
+		int j = 20;
+
+		std::cout << "int i is " << i << std::endl;
+		std::cout << "int j is " << j << std::endl;
+		std::cout << "Lesser value is " << Compare(i, j) << "\n" << std::endl;
+	}
+
+	/// floatŒ^‚Ì”äŠr
+	{
+		float i = 10.0f;
+		float j = 20.0f;
+
+		std::cout << "float i is " << std::fixed << std::setprecision(1) << i << "f" << std::endl;
+		std::cout << "float j is " << std::fixed << std::setprecision(1) << j << "f" << std::endl;
+		std::cout << "Lesser value is " << std::fixed << std::setprecision(1) << Compare(i, j) << "f" << "\n" << std::endl;
+	}
+
+	/// doubleŒ^‚Ì”äŠr
+	{
+		double i = 10.0;
+		double j = 20.0;
+
+		std::cout << "double i is " << std::fixed << std::setprecision(6) << i << std::endl;
+		std::cout << "double j is " << std::fixed << std::setprecision(6) << j << std::endl;
+		std::cout << "Lesser value is " << std::fixed << std::setprecision(6) << Compare(i, j) << "\n" << std::endl;
+	}
+
+
 	return 0;
 }

e96fe93b603028655054fd51b8679bc410ab282e
keno555552 <k024g1024@g.neec.ac.jp>
2025-12-18 19:23:38 +0900

    03-02 v1.0

--- 03-02/main.cpp
+++ 03-02/main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
 #include <stdio.h>
 
+/// クラステンプレート定義
+template<typename T>
+class Comparator {
+public:
+
+	T a;
+	T b;
+
+	/// コンストラクタ
+	Comparator(T first, T second):a(first), b(second) {};
+
+	/// メンバ関数
+	T Greater() {
+		if (a > b) {
+			return static_cast<T>(a);
+		} else {
+			return static_cast<T>(b);
+		}
+	}
+
+};
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	
+	/// int型での利用
+	int a = 5;
+	int b = 10;
+	Comparator<int> comp(a,b);
+	printf("Greater value between %d and %d is %d\n", a,b,comp.Greater());
+	
+	/// float型での利用
+	float c = 5;
+	float d = 10;
+	Comparator<float> comp2(c,d);
+	printf("Greater value between %.2ff and %.2ff is %.2ff\n", c,d,comp2.Greater());
+	
+	/// double型での利用
+	double e = 5;
+	double f = 10;
+	Comparator<double> comp3(e,f);
+	printf("Greater value between %lf and %lf is %lf\n", e,f,comp3.Greater());
+
 	return 0;
 }
